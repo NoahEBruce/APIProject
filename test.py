5589f219a483e6d2255d0a12cb8fd33ce2b5ad22
Noah <123440074+NoahEBruce@users.noreply.github.com>
2023-08-12 15:06:43 -0500

    Update test.py

--- test.py
+++ test.py
@@ -6,12 +6,18 @@ import redis
 from redis import Redis, RedisError
 import sys
 
-COMMAND = sys.argv[1:]
+n = len(sys.argv)
+
+if n < 3:
+    print("Usage: python script.py arg1 arg2")
+    sys.exit(1)
+    
+COMMAND = sys.argv[1]
 COMMAND2 = sys.argv[2]
 
 print(" ")
 
-n = len(sys.argv)
+
 
 for x in range(0, n):
     print(sys.argv[x], x)
