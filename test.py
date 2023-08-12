1ca6fb0864f56796e803d3d311b55c78f5eae102
Noah <123440074+NoahEBruce@users.noreply.github.com>
2023-08-12 15:49:50 -0500

    Update test.py

--- test.py
+++ test.py
@@ -6,6 +6,9 @@ import redis
 from redis import Redis, RedisError
 import sys
 
+COMMAND = sys.argv[1]
+COMMAND2 = sys.argv[2]
+
 print(" ")
 
 n = len(sys.argv)
@@ -13,20 +16,13 @@ n = len(sys.argv)
 for x in range(0, n):
     print(sys.argv[x], x)
 
+
 print(" ")
 
-if len(sys.argv) < 3:
-    print("Please provide at least two command-line arguments.")
-    sys.exit(1)
-
-COMMAND = sys.argv[1]
-COMMAND2 = sys.argv[2]
-
-print("Input 1:", COMMAND)
-print("Input 2:", COMMAND2)
-
 s = COMMAND2
+
 md5_hash = hashlib.md5(s.encode()).hexdigest()
 
 print("input:", s)
 print("output:", md5_hash)
+
